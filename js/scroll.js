b5e7bdd1b2cacbac0614999fa72ae293fcc3dda9
guzmandp <guzmandp@gmail.com>
2022-10-15 19:59:36 -0300

    UpDate Responsive

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,8 @@
 (function () {
-    var navBar = document.getElementById('nav'); // colocar em cache
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 100) navBar.classList.add('corNav'); // > 0 ou outro valor desejado
-        else navBar.classList.remove('corNav');
-    });
+  var navBar = document.getElementById("nav"); // colocar em cache
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 1)
+      navBar.classList.add("corNav"); // > 0 ou outro valor desejado
+    else navBar.classList.remove("corNav");
+  });
 })();
-
-
-
- 
