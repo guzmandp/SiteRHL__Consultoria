e3c356a52ee88ac92c2bae9ce029c49148c76850
guzmandp <guzmandp@gmail.com>
2023-01-30 23:40:05 -0300

    update responsive

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,16 @@
-(function () {
+/* (function () {
   var navBar = document.getElementById("nav"); // colocar em cache
   window.addEventListener("scroll", function () {
     if (window.scrollY > 1)
       navBar.classList.add("corNav"); // > 0 ou outro valor desejado
     else navBar.classList.remove("corNav");
   });
-})();
+})(); */
+
+
+
+document
+  .querySelector(".hamburguer")
+  .addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+  );
